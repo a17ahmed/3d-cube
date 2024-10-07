847054a0e1a1e998c589335156952532304bb33a
Ahmed irfan <a17irfan@gmail.com>
2024-10-07 22:04:20 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.scss';
+import { Route, Routes } from 'react-router-dom';
+// import Layout from './components/layout/layout';
+
+import About from './components/About/about';
+
+
+function App() {
+  return (
+    <>
+   <About/>
+    </>
+  );
+}
+
+export default App;
